2fd63d2a995ffb4416853b4f4a898c8f09ab2ac9
imadenigma <imadwydad7@gmail.com>
2020-10-03 22:23:53 +0100

    Monk

---
+++ src/main/java/me/aiglez/lonkskit/abilities/ImadAbilities/itembased/Monk.java
@@ -0,0 +1,67 @@
+package me.aiglez.lonkskit.abilities.ImadAbilities.itembased;
+
+
+import me.aiglez.lonkskit.abilities.ItemStackAbility;
+import me.aiglez.lonkskit.players.LocalPlayer;
+import me.lucko.helper.config.ConfigurationNode;
+import me.lucko.helper.item.ItemStackBuilder;
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Monk extends ItemStackAbility {
+    ItemStack item;
+
+    protected Monk(ConfigurationNode configuration) {
+        super("monk", configuration);
+        this.item = ItemStackBuilder.of(Material.BLAZE_ROD).name("&6Wand").build();
+    }
+
+    @Override
+    public ItemStack getItemStack() {
+        return item;
+    }
+
+    @Override
+    public boolean isItemStack(ItemStack item) {
+        return item.isSimilar(this.item);
+    }
+
+    @Override
+    public void whenClicked(PlayerInteractEvent e) {
+    }
+    @EventHandler
+    public void whenClickPlayer(PlayerInteractEntityEvent event){
+        LocalPlayer player = LocalPlayer.get(event.getPlayer());
+        if (!this.cooldown.test(player)){
+            player.msg("&cPlease wait, {0} second(s) left", new Object[]{this.cooldown.remainingTime(player, TimeUnit.SECONDS)});
+
+        } else {
+            if (event.getRightClicked() instanceof Player) {
+                Player target = (Player) event.getRightClicked();
+                int a = new Random().nextInt(30);
+                if (target.getInventory().getItem(a).getType() != Material.AIR) {
+                    ItemStack first = target.getItemInHand();
+                    ItemStack second = target.getInventory().getItem(a);
+                    target.setItemInHand(second);
+                    target.getInventory().setItem(a, first);
+                }
+            }
+        }
+    }
+}
+/*Leather Body, leather leggings, letherboots
+<-- all unbreaking III, Protection III. Wood Sword:Unbreaking III Sharpness II. Blaze Rod. 34 soup.
+(RENAME blaze rod 'Staff')
+(If a player right clicks another player with a blaze rod, the target player's
+item goes into their inventory in a random slot and the item in their hand is set to air
+. If the target player's inventory is full, it swaps a random item in the target player's inventory with the item in their hand.)(5 second cooldown)*/
