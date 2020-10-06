5f46429fdd2e411bb8fe95c1198c00422964a70f
AigleDev <eaglezledozo@gmail.com>
2020-10-07 00:47:49 +0100

    Update all files for TravisCI

--- src/main/java/me/aiglez/lonkskit/exceptions/AbilityRegisterException.java
+++
@@ -1,9 +0,0 @@
-package me.aiglez.lonkskit.exceptions;
-
-public class AbilityRegisterException extends RuntimeException {
-
-    public AbilityRegisterException(String abilityName, String reason) {
-        super("Couldn't load the ability with name: " + abilityName + "\n" +
-                "Reason: " + reason);
-    }
-}
