93d6a340ce05b38a3f5014ba4adb07e424404541
AigleDev <eaglezledozo@gmail.com>
2020-10-07 00:41:44 +0100

    MetadataProvider cleanup

--- src/main/java/me/aiglez/lonkskit/utils/MetadataProvider.java
+++ src/main/java/me/aiglez/lonkskit/utils/MetadataProvider.java
@@ -10,12 +10,11 @@ public class MetadataProvider {
     public static final MetadataKey<Boolean> PLAYER_NO_FALL_DAMAGE = MetadataKey.createBooleanKey("no-fall-damage");
     public static final MetadataKey<Boolean> PLAYER_NO_LIGHTING_DAMAGE = MetadataKey.createBooleanKey("no-lightning-damage");
 
-    public static final MetadataKey<Boolean> HORSE_PERSISTENT = MetadataKey.createBooleanKey("persistent-horse");
-
     /**
      * Entities MetadataProvider
      */
     public static final MetadataKey<Boolean> SNOWBALL_EXPLODE = MetadataKey.createBooleanKey("snowball-explode");
+    public static final MetadataKey<Boolean> HORSE_PERSISTENT = MetadataKey.createBooleanKey("persistent-horse");
 
     private MetadataProvider() {
         throw new UnsupportedOperationException();
