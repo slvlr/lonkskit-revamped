88570fe6993af5a32122b2950477c8b18125ea93
AigleDev <eaglezledozo@gmail.com>
2020-10-18 11:00:14 +0100

    Spy revert to using Bukkit's spectator target api

--- src/main/java/me/aiglez/lonkskit/abilities/itembased/SpyAbility.java
+++ src/main/java/me/aiglez/lonkskit/abilities/itembased/SpyAbility.java
@@ -1,20 +1,21 @@
 package me.aiglez.lonkskit.abilities.itembased;
 
+import com.destroystokyo.paper.event.player.PlayerStopSpectatingEntityEvent;
 import me.aiglez.lonkskit.abilities.AbilityPredicates;
 import me.aiglez.lonkskit.abilities.ItemStackAbility;
 import me.aiglez.lonkskit.players.LocalPlayer;
+import me.aiglez.lonkskit.utils.Logger;
 import me.aiglez.lonkskit.utils.MetadataProvider;
 import me.lucko.helper.Events;
-import me.lucko.helper.Schedulers;
 import me.lucko.helper.config.ConfigurationNode;
+import me.lucko.helper.event.filter.EventFilters;
 import me.lucko.helper.metadata.SoftValue;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author AigleZ
@@ -45,42 +46,15 @@ public class SpyAbility extends ItemStackAbility {
                 .handler(e -> {
                     final LocalPlayer localPlayer = LocalPlayer.get((Player) e.getEntity());
                     final Arrow arrow = (Arrow) e.getProjectile();
+                    arrow.setShooter(localPlayer.toBukkit());
 
                     applyEffects(localPlayer);
 
                     // spectator - start
-                    arrow.remove();
 
                     localPlayer.metadata().put(MetadataProvider.SPY_PLAYER, SoftValue.of(true));
                     localPlayer.toBukkit().setGameMode(GameMode.SPECTATOR);
-                    localPlayer.toBukkit().setVelocity(
-                            localPlayer.getLocation().getDirection().multiply(5.0 * e.getForce()));
-
-                    localPlayer.toBukkit().setNoDamageTicks(10); // no damage
-
-                    final AtomicInteger passed = new AtomicInteger(0);
-                    Schedulers.sync()
-                            .runRepeating(t -> {
-                                if(localPlayer.getLocation().subtract(0D, 1D, 0D).getBlock().getType().isSolid() && passed.intValue() > 20) {
-                                    localPlayer.toBukkit().setNoDamageTicks(0);
-                                    localPlayer.toBukkit().setFallDistance(0);
-
-                                    localPlayer.metadata().remove(MetadataProvider.SPY_PLAYER);
-
-                                    localPlayer.toBukkit().setGameMode(GameMode.SURVIVAL);
-                                    localPlayer.toBukkit().teleport(localPlayer.getLocation().subtract(0D, 1D, 0D));
-
-                                    localPlayer.msg("&6(Spy - Debug) &cYou are no longer spying (// you have landed)");
-                                    t.stop();
-                                    return;
-                                }
-
-                                localPlayer.toBukkit().setNoDamageTicks(10);
-                                localPlayer.toBukkit().setFallDistance(0);
-
-                                passed.addAndGet(2);
-                            }, 5L, 2L);
-
+                    localPlayer.toBukkit().setSpectatorTarget(arrow);
                     // spectator - end
 
                     localPlayer.msg("&6(Spy - Debug) &eYou are now spying on " + arrow.getName());
@@ -93,29 +67,40 @@ public class SpyAbility extends ItemStackAbility {
                     e.getPlayer().sendMessage("§6(Spy - Debug) §cYou can't interact while spying!");
                     e.setCancelled(true);
                 });
+
+        Events.subscribe(PlayerStopSpectatingEntityEvent.class)
+                .filter(AbilityPredicates.hasAbility(this))
+                .filter(AbilityPredicates.hasMetadata(MetadataProvider.SPY_PLAYER))
+                .handler(e -> {
+                    e.getPlayer().sendMessage("§6(Spy - Debug) §cWait until the arrow lands!");
+                    e.setCancelled(true);
+                });
+
+        Events.subscribe(ProjectileHitEvent.class)
+                .filter(e -> e.getEntity() instanceof Arrow)
+                .filter(EventFilters.entityHasMetadata(MetadataProvider.SPY_ARROW))
+                .handler(e -> {
+                    final Arrow arrow = (Arrow) e.getEntity();
+                    if(arrow.getShooter() == null) {
+                        Logger.debug("[Spy] The arrow doesn't have a shooter!");
+                        return;
+                    }
+
+                    final LocalPlayer localPlayer = LocalPlayer.get((Player) arrow.getShooter());
+
+                    localPlayer.toBukkit().setSpectatorTarget(null);
+                    localPlayer.toBukkit().setGameMode(GameMode.SURVIVAL);
+
+                    localPlayer.toBukkit().teleport(arrow.getLocation().subtract(0D, 1D, 0D));
+                    localPlayer.metadata().remove(MetadataProvider.SPY_PLAYER);
+
+                    localPlayer.msg("&6(Spy - Debug) &aYour arrow has landed");
+                });
     }
 
 
     /*
-    Events.subscribe(ProjectileHitEvent.class)
-                .filter(e -> e.getEntity() instanceof Arrow)
-                .filter(EventFilters.entityHasMetadata(MetadataProvider.SPY_ARROW))
-                .handler(e -> {
-                   final Arrow arrow = (Arrow) e.getEntity();
-                   if(arrow.getShooter() == null) {
-                       Logger.debug("[Spy] The arrow doesn't have a shooter!");
-                       return;
-                   }
 
-                   final LocalPlayer localPlayer = LocalPlayer.get((Player) arrow.getShooter());
-                   localPlayer.msg("&6(Spy - Debug) &aYour arrow has landed");
-
-                   localPlayer.toBukkit().setSpectatorTarget(null);
-                   localPlayer.toBukkit().setGameMode(GameMode.SURVIVAL);
-
-                    localPlayer.toBukkit().teleport(arrow.getLocation().subtract(0D, 1D, 0D));
-                    localPlayer.metadata().remove(MetadataProvider.SPY_PLAYER);
-                });
 
     arrow.setShooter(localPlayer.toBukkit());
                     Metadata.provideForEntity(arrow).put(MetadataProvider.SPY_ARROW, SoftValue.of(true));
