53ec9df068964a21c5246cc8ac4162c9a5fc3a31
AigleDev <eaglezledozo@gmail.com>
2020-10-18 15:18:54 +0100

    Since i don't have any way to test the Cowboy ability, i'm gonna let it until i'm done with all abilities

--- src/main/java/me/aiglez/lonkskit/WorldProvider.java
+++ src/main/java/me/aiglez/lonkskit/WorldProvider.java
@@ -1,12 +1,17 @@
 package me.aiglez.lonkskit;
 
+import me.lucko.helper.Helper;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 
+import javax.annotation.Nonnull;
+
 public class WorldProvider {
 
-    public static final World KP_WORLD = Bukkit.getWorld("kitpvp");
+    @Nonnull
+    public static final World KP_WORLD = Helper.world("kitpvp").orElseThrow(() -> new IllegalStateException("Couldn't find any world matching the name [kitpvp]"));
     public static final World MAIN_WORLD = Bukkit.getWorld("world");
+
 
 
     private WorldProvider() {
--- src/main/java/me/aiglez/lonkskit/abilities/itembased/CowboyAbility.java
+++ src/main/java/me/aiglez/lonkskit/abilities/itembased/CowboyAbility.java
@@ -42,6 +42,15 @@ public class CowboyAbility extends ItemStackAbility {
 
     @Override
     public void registerListeners() {
+        handleHorseListeners();
+        handleLassoListeners();
+    }
+
+    private void handleLassoListeners() {
+
+    }
+
+    private void handleHorseListeners() {
         // spawn horse
         Events.subscribe(KitSelectEvent.class)
                 .filter(e -> e.getKit().hasAbility(this))
@@ -82,15 +91,13 @@ public class CowboyAbility extends ItemStackAbility {
                 .filter(e -> Metadata.provideForEntity(e.getVehicle()).has(MetadataProvider.HORSE_PERSISTENT))
                 .handler(e -> {
                     final LocalPlayer localPlayer = LocalPlayer.get((Player) e.getExited());
-                    if(localPlayer.metadata().has(MetadataProvider.LEAVE_HORSE)) {
-                        return;
-                    }
-
                     localPlayer.msg("&6(Cowboy) &cYou can't dismount your horse! (Exit Event)");
 
                     // we remount him back, because of spigot's 1.16+ issue with mounting
-                    Schedulers.sync()
-                            .runLater(() -> e.getVehicle().addPassenger(e.getExited()), 2L);
+                    if(!localPlayer.toBukkit().isDead()) {
+                        Schedulers.sync()
+                                .runLater(() -> e.getVehicle().addPassenger(e.getExited()), 2L);
+                    }
                 });
 
         // kill player's horse when he dies.
@@ -98,21 +105,25 @@ public class CowboyAbility extends ItemStackAbility {
                 .filter(AbilityPredicates.possiblyHasAbility(this))
                 .handler(e -> {
                     final LocalPlayer localPlayer = LocalPlayer.get((Player) e.getEntity());
+                    if(localPlayer.toBukkit().isInsideVehicle()) {
+                        localPlayer.toBukkit().getVehicle().remove();
+                        localPlayer.msg("&6(Cowboy) &cSince you died, your horse died too.");
+                    }
 
-                    localPlayer.metadata().put(MetadataProvider.LEAVE_HORSE, SoftValue.of(true));
-                    localPlayer.toBukkit().leaveVehicle();
 
+                    /*
                     WorldProvider.KP_WORLD.getNearbyEntitiesByType(Horse.class, localPlayer.getLocation(), 5, 5, 5)
                             .forEach(horse -> {
                                 if(Metadata.provideForEntity(horse).has(MetadataProvider.HORSE_PERSISTENT)) {
                                     final LocalPlayer owner = Metadata.provideForEntity(horse).getOrNull(MetadataProvider.HORSE_PERSISTENT);
                                     if(localPlayer.getUniqueId().equals(owner.getUniqueId())) {
-                                        horse.damage(999);
                                         horse.remove();
                                         localPlayer.msg("&6(Cowboy) &cSince you died, your horse died too.");
                                     }
                                 }
                             });
+
+                     */
                 });
     }
 }
